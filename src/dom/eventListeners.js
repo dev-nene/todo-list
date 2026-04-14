cc26b725b1e21d464b0c33dcbd176ce0510ebdae
Nemanja Pavlovic <268587348+dev-nene@users.noreply.github.com>
2026-04-14 09:10:12 +0200

    Add delete todo functionality

--- src/dom/eventListeners.js
+++ src/dom/eventListeners.js
@@ -13,7 +13,12 @@ function checkTodo(todos, e) {
 }
 
 function deleteTodo(todos, e) {
+  const id = e.target.dataset.id;  
+  if (!id) return;
 
+  const updatedTodos  = todos.filter((todo) => String(todo.id) !== id);
+
+  setTodos(updatedTodos);
 }
 
 function expandTodo(todos, e) {}
