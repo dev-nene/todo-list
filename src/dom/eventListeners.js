19801a368d59b2126330c85db5c78eefaef41d2c
Nemanja Pavlovic <268587348+dev-nene@users.noreply.github.com>
2026-04-15 11:05:50 +0200

    Fix form reset after creating todo

--- src/dom/eventListeners.js
+++ src/dom/eventListeners.js
@@ -14,31 +14,50 @@ function checkTodo(todos, e) {
 }
 
 function deleteTodo(todos, e) {
-  const id = e.target.dataset.id;  
+  const id = e.target.dataset.id;
   if (!id) return;
 
-  const updatedTodos  = todos.filter((todo) => String(todo.id) !== id);
+  const updatedTodos = todos.filter((todo) => String(todo.id) !== id);
 
   setTodos(updatedTodos);
 }
 
 function editTodo(todos, e) {
-  
+
 }
 
-function expandTodo(todos, e) {
-  
-}
+function expandTodo(todos, e) {}
 
 function makeTodo() {
-    const title = document.querySelector("#title").value;
-    const desc = document.querySelector("#desc").value;
-    const dueDate = document.querySelector("#dueDate").value;
-    const priority = document.querySelector("#priority").value;
-    const form = document.querySelector(".todo-form")
-    form.reset();
-    const todo = createTodo(title, desc, dueDate, priority);
-    addTodo(todo)
+  const title = document.querySelector("#title").value;
+  const desc = document.querySelector("#desc").value;
+  const dueDate = document.querySelector("#dueDate").value;
+  const priority = document.querySelector("#priority").value;
+  const form = document.querySelector(".todo-form");
+
+  if (!title) {
+    alert("Title is required");
+    return;
+  }
+
+  if (!desc) {
+    alert("Description is required");
+    return;
+  }
+
+  if (!dueDate) {
+    alert("Date is required");
+    return;
+  }
+
+  if (!priority) {
+    alert("Priority is required");
+    return;
+  }
+
+  const todo = createTodo(title, desc, dueDate, priority);
+  addTodo(todo);
+  form.reset();
 }
 
 export { checkTodo, deleteTodo, expandTodo, editTodo, makeTodo };
